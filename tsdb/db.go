066c0aef64592b323d1e4be2aa56aa061981e1ce
hit9 <hit9@icloud.com>
2015-12-07 14:40:06 +0800

    Clean directory

--- tsdb/db.go
+++ tsdb/db.go
@@ -1,94 +1,41 @@
-// Copyright (c) 2015 MIT. Eleme, Inc.
 // Package tsdb implements an embededable time-series and hash-table
 // storage based on leveldb.
 
 package tsdb
 
 import (
-	"strconv"
-
 	"github.com/syndtr/goleveldb/leveldb"
+	leveldbOpt "github.com/syndtr/goleveldb/leveldb/opt"
 )
 
-// Time stamps are uint64 numbers (in seconds)
+// A TimeStamp is auint64 number.
 type TimeStamp uint64
 
-// Series values are float64 numbers
+// A Key is a string.
+type Key string
+
+// A Value is a float64 number.
 type Value float64
 
-// DB is a tsdb database
+// DB is a tsdb database.
 type DB struct {
-	// Leveldb handle
+	// leveldb handle
 	db *leveldb.DB
 }
 
-// Default value for series name key.
-const DefaultNameValue Value = 0
-
-// Open or create a DB for the given path.
-func OpenFile(fileName string, options leveldb.Options) (*DB, error) {
-	db, err := leveldb.OpenFile(fileName, options)
+// Open a DB for given path, directory will be created if the path dose
+// not exist.
+func (db *DB) OpenFile(fileName string, options leveldbOpt.Options) (*DB, error) {
+	ldb, err := leveldb.OpenFile(fileName, options)
 	if err != nil {
 		return nil, err
 	}
-	return &DB{db: db}
+	db := new(DB)
+	db.db = ldb
+	return db
 }
 
 // Close a DB.
-func (db *DB) Close() {
-	db.db.Close()
-}
-
-// Put datapoint into db with name, time stamp and value. This will
-// put 2 key into leveldb: name to 0 and time stamp to value.
-func (db *DB) Put(name string, t TimeStamp, v Value) error {
-	return PutWithNameValue(name, DefaultNameValue, t, v)
-}
-
-// Put time series name with a value, this enables using series name to
-// value as a hash table.
-func (db *DB) PutName(name string, v Value) error {
-	key := encodeSeriesNameKey(name)
-	val := encodeValue(v)
-	return db.db.Put([]byte(key), []byte(val), nil)
-}
-
-// Put datapoint into db with name, time stamp and value, additionally
-// with the value for series name (default as 0).
-func (db *DB) PutWithNameValue(name string, nameValue Value, t TimeStamp, v Value) error {
-	err := db.PutName(name, nameValue)
-	if err != nil {
-		return err
-	}
-	key := encodeTimeStampKey(name, t)
-	val := encodeValue(v)
-	err = db.db.Put([]byte(key), []byte(val), nil)
-	if err != nil {
-		db.DeleteName(name)
-		return err
-	}
-	return nil
-}
-
-// Delete time series name from db.
-func (db *DB) DeleteName(name string) error {
-	key := encodeSeriesNameKey(name)
-	err := db.db.Delete([]byte(key), nil)
-	return err
-}
-
-// Delete datapoint from db by name and timestamp. If this is the last
-// datapoint in the series, the series name will be deleted too.
-func (db *DB) Delete(name string, t TimeStamp) error {
-	// NotImplemented
-}
-
-// Get datapoint value from db by name and timestamp.
-func (db *DB) Get(name string, t TimeStamp) (Value, error) {
-	// NotImplemented
-}
-
-// Get the value of series name (as key).
-func (db *DB) GetNameValue(name string) (Value, error) {
-	// NotImplemented
+func (db *DB) Close() error {
+	return db.db.Close()
 }
--- tsdb/errors.go
+++
@@ -1,8 +0,0 @@
-package tsdb
-
-import "errors"
-
-var (
-	// ErrKeyFormat is returned if the key is invalid when being decode.
-	ErrKeyFormat = errors.New("Invalid db key format")
-)
