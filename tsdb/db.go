40c8ca3fa5cbd1893a31cb478c484e2d70903f70
hit9 <hit9@icloud.com>
2015-12-07 10:58:51 +0800

    Add Put related methods

--- tsdb/db.go
+++ tsdb/db.go
@@ -41,19 +41,50 @@ func (db *DB) Put(name string, t TimeStamp, v Value) error {
 	return PutWithNameValue(name, DefaultNameValue, t, v)
 }
 
+// Put time series name with a value, this enables using series name to
+// value as a hash table.
+func (db *DB) PutName(name string, v Value) error {
+	key := encodeSeriesNameKey(name)
+	val := encodeValue(v)
+	return db.db.Put([]byte(key), []byte(val), nil)
+}
+
 // Put datapoint into db with name, time stamp and value, additionally
 // with the value for series name (default as 0).
 func (db *DB) PutWithNameValue(name string, nameValue Value, t TimeStamp, v Value) error {
-	ns := encodeSeriesNameKey(name)
-	err := db.db.Put(ns, []byte(encodeValue(nameValue)), nil)
+	err := db.PutName(name, nameValue)
 	if err != nil {
 		return err
 	}
-	ts := encodeTimeStampKey(name, t)
-	err := db.db.Put(ts, []byte(encodeValue(v)), nil)
+	key := encodeTimeStampKey(name, t)
+	val := encodeValue(v)
+	err = db.db.Put([]byte(key), []byte(val), nil)
 	if err != nil {
-		db.db.Delete(ns, nil)
+		db.DeleteName(name)
 		return err
 	}
 	return nil
+}
+
+// Delete time series name from db.
+func (db *DB) DeleteName(name string) error {
+	key := encodeSeriesNameKey(name)
+	err := db.db.Delete([]byte(key), nil)
+	return err
+}
+
+// Delete datapoint from db by name and timestamp. If this is the last
+// datapoint in the series, the series name will be deleted too.
+func (db *DB) Delete(name string, t TimeStamp) error {
+	// NotImplemented
+}
+
+// Get datapoint value from db by name and timestamp.
+func (db *DB) Get(name string, t TimeStamp) (Value, error) {
+	// NotImplemented
+}
+
+// Get the value of series name (as key).
+func (db *DB) GetNameValue(name string) (Value, error) {
+	// NotImplemented
 }
