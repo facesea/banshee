4879c7b29547bc09cdcc71a44fa7a6fdb8e0c31a
hit9 <hit9@icloud.com>
2015-12-08 00:52:21 +0800

    Rebuild the db

--- tsdb/db.go
+++ tsdb/db.go
@@ -4,36 +4,86 @@
 package tsdb
 
 import (
+	"hash/fnv"
 	"sync"
+	"sync/atomic"
 
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 )
 
+// Number of write locks
+const nWLocks = 10000
+
 // DB is a tsdb database.
 type DB struct {
-	db        *leveldb.DB
-	tsLenLock *sync.Mutex
-	hLenLock  *sync.Mutex
+	// LevelDB
+	lv *leveldb.DB
+	// Write Lock
+	wls [nWLocks]*sync.Mutex
+	// Close
+	closed uint32
 }
 
-// Open a DB for given path, directory will be created if the path dose
-// not exist. example:
-//	db := OpenFile("mydb", nil)
-//	defer db.Close()
+// Open a DB for the given path, the DB will be created if not exist. Note
+// that the DB can be opened for only once.
+//
+// OpenFile will return an error with type of ErrCorrupted if corruption
+// detected in the DB.
+//
+// The DB instance is goroutine-safe.
 func OpenFile(fileName string, options *opt.Options) (*DB, error) {
-	ldb, err := leveldb.OpenFile(fileName, options)
+	lv, err := leveldb.OpenFile(fileName, options)
 	if err != nil {
 		return nil, NewErrCorrupted(err)
 	}
 	db := new(DB)
-	db.db = ldb
-	db.tsLenLock = &sync.Mutex{}
-	db.hLenLock = &sync.Mutex{}
+	db.lv = db
+	for i := 0; i < nWLocks; i++ {
+		db.wls[i] = new(sync.Mutex)
+	}
+	db.closed = 0
 	return db, nil
 }
 
 // Close a DB.
 func (db *DB) Close() error {
+	if db.isClosed() {
+		return ErrClosed()
+	}
 	return db.db.Close()
+}
+
+// Check whether DB was closed.
+func (db *DB) isClosed() bool {
+	return atomic.LoadUint32(&db.closed) != 0
+}
+
+// Check whether DB is ok.
+func (db *DB) ok() error {
+	if db.isClosed() {
+		return ErrClosed
+	}
+	return nil
+}
+
+// Acquire a write lock by key. All leveldb keys share a certain amount of
+// write locks within one DB.
+//
+// Write locks do not often be used, except some operations like `incr`, which
+// requires atomic write.
+func (db *DB) acquireWLock(key string) *sync.Mutex {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	i := h.Sum32() % nWLocks
+	return db.wls[i]
+}
+
+// Batch creates a write batch, which enables write multiple operations once.
+// Note the returned Batch instance is NOT goroutine-safe.
+func (db *DB) Batch() *Batch {
+	b := new(Batch)
+	b.db = db
+	b.lb = new(leveldb.Batch)
+	return b
 }
