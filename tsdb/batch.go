f6a35474e736ae12e2baac957aa87aa6d509d17d
hit9 <hit9@icloud.com>
2015-12-08 15:00:14 +0800

    Add batch put

--- tsdb/batch.go
+++ tsdb/batch.go
@@ -9,18 +9,37 @@ type Batch struct {
 	// DB handle
 	db *DB
 	// LevelDB batch
-	lb *leveldb.Batch
+	b *leveldb.Batch
 }
 
 // NewBatch creates a write batch on a DB instance.
 func NewBatch(db *DB) *Batch {
 	b := new(Batch)
 	b.db = db
-	b.lb = new(leveldb.Batch)
+	b.b = new(leveldb.Batch)
 	return b
 }
 
-// Add Put operation to the write batch.
-func (b *Batch) Put(name string, t uint64, v float64) {
-	//
+// Reset batch.
+func (b *Batch) Reset() {
+	b.b.Reset()
+}
+
+// Flush write batch to db.
+func (b *Batch) Flush() {
+	b.db.db.Write(b.b, nil)
+}
+
+// Put stamp with value and score to db.
+func (b *Batch) PutStamp(name string, stamp uint64, value float64, score float64) {
+	key := encodeStampKey(name, stamp)
+	val := encodeStampValue(value, score)
+	b.b.Put([]byte(key), []byte(val))
+}
+
+// Put name with trend and stamp.
+func (b *Batch) PutName(name string, trend float64, stamp uint64) {
+	key := encodeNameKey(name)
+	val := encodeNameValue(trend, stamp)
+	b.b.Put([]byte(key), []byte(val))
 }
