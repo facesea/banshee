952dc5fd5563f9995c17f82e0f71ad6f83af63db
hit9 <hit9@icloud.com>
2015-12-11 13:40:46 +0800

    Add safemap

---
+++ util/safemap.go
@@ -0,0 +1,75 @@
+// Copyright 2015 Eleme Inc. All rights reserved.
+
+package util
+
+import "sync"
+
+// SafeMap is a map like container with read/write lock to keep goroutine
+// safety.
+type SafeMap struct {
+	lock *sync.RWMutex
+	m    map[interface{}]interface{}
+}
+
+// NewSafeMap creates a SafeMap.
+func NewSafeMap() *SafeMap {
+	return &SafeMap{
+		lock: &sync.RWMutex{},
+		m:    make(map[interface{}]interface{}),
+	}
+}
+
+// Get value from map by key.
+func (m *SafeMap) Get(key interface{}) (interface{}, bool) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	if val, ok := m.m[key]; ok {
+		return val, true
+	}
+	return nil, false
+}
+
+// Set value to map by key.
+func (m *SafeMap) Set(key, val interface{}) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	m.m[key] = val
+}
+
+// Check if a key is in map.
+func (m *SafeMap) Has(key interface{}) bool {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	_, ok := m.m[key]
+	return ok
+}
+
+// Delete a value from map by key.
+func (m *SafeMap) Delete(key interface{}) bool {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	_, ok := m.m[key]
+	if ok {
+		delete(m.m, key)
+		return true
+	}
+	return false
+}
+
+// Get all items in the map.
+func (m *SafeMap) Items() map[interface{}]interface{} {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	d := make(map[interface{}]interface{})
+	for key, val := range m.m {
+		d[key] = val
+	}
+	return d
+}
+
+// Get map length.
+func (m *SafeMap) Len() int {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	return len(m.m)
+}
