75f024bfc72a19b89f77d0e1867a231f67ca7b28
hit9 <hit9@icloud.com>
2015-12-13 16:04:02 +0800

    Add simple detector.detect implementation

--- detector/detector.go
+++ detector/detector.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/eleme/banshee/algorithm"
 	"github.com/eleme/banshee/config"
 	"github.com/eleme/banshee/models"
 	"github.com/eleme/banshee/storage"
@@ -20,8 +21,12 @@ import (
 
 // Detector is a tcp server to detect anomalies.
 type Detector struct {
+	// Debug
+	debug bool
 	// Config
 	cfg *config.Config
+	// Logger
+	logger *util.Logger
 	// Storage
 	db *storage.DB
 	// Rules
@@ -30,12 +35,15 @@ type Detector struct {
 	rulesNames map[string][]string
 }
 
-var logger = util.NewLogger("detector")
-
 // Init new Detector.
-func New(cfg *config.Config, db *storage.DB) *Detector {
+func New(debug bool, cfg *config.Config, db *storage.DB) *Detector {
 	detector := new(Detector)
+	detector.debug = debug
 	detector.cfg = cfg
+	detector.logger = util.NewLogger("detector")
+	if detector.debug {
+		detector.logger.SetLevel(util.LOG_DEBUG)
+	}
 	detector.db = db
 	detector.rulesCache = util.NewSafeMap()
 	detector.rulesNames = map[string][]string{}
@@ -48,13 +56,13 @@ func (detector *Detector) Start() {
 	addr := fmt.Sprintf("0.0.0.0:%d", detector.cfg.Detector.Port)
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
-		logger.Fatal("failed to bind tcp://%s: %v", addr, err)
+		detector.logger.Fatal("failed to bind tcp://%s: %v", addr, err)
 	}
-	logger.Info("listening on tcp://%s..", addr)
+	detector.logger.Info("listening on tcp://%s..", addr)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			logger.Fatal("failed to accept new conn: %v", err)
+			detector.logger.Fatal("failed to accept new conn: %v", err)
 		}
 		go detector.handle(conn)
 	}
@@ -66,12 +74,12 @@ func (detector *Detector) handle(conn net.Conn) {
 	addr := conn.RemoteAddr()
 	defer func() {
 		conn.Close()
-		logger.Info("conn %s disconnected", addr)
+		detector.logger.Info("conn %s disconnected", addr)
 	}()
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
-			logger.Info("failed to read conn: %v, closing it..", err)
+			detector.logger.Info("failed to read conn: %v, closing it..", err)
 			break
 		}
 		line := scanner.Text()
@@ -80,16 +88,16 @@ func (detector *Detector) handle(conn net.Conn) {
 			if len(line) > 10 {
 				line = line[:10]
 			}
-			logger.Error("failed to parse '%s': %v, skipping..", line, err)
+			detector.logger.Error("failed to parse '%s': %v, skipping..", line, err)
 			continue
 		}
 		if detector.match(m) {
 			err = detector.detect(m)
 			if err != nil {
-				logger.Error("failed to detect metric: %v, skipping..", err)
+				detector.logger.Error("failed to detect metric: %v, skipping..", err)
 				continue
 			}
-			logger.Info("%s %.3f", m.Name, m.Score)
+			detector.logger.Debug("detected %s => average %.3f, socre %.3f", m.Name, m.Average, m.Score)
 		}
 	}
 }
@@ -128,6 +136,33 @@ func (detector *Detector) match(m *models.Metric) bool {
 
 // Detect incoming metric with 3-sigma rule and fill the metric.Score.
 func (detector *Detector) detect(m *models.Metric) error {
-	// TODO
-	return nil
+	wf := detector.cfg.Detector.TrendFactor
+	startSize := detector.cfg.Detector.StartSize
+	state, err := detector.db.GetState(m)
+	if err != nil && err != storage.ErrNotFound {
+		return err
+	}
+	stateNext := &models.State{}
+	if err == storage.ErrNotFound {
+		m.Average = m.Value
+		stateNext.Average = m.Value
+		stateNext.StdDev = 0
+		stateNext.Count = 1
+	} else {
+		m.Average = state.Average
+		stateNext.Average = algorithm.Ewma(wf, state.Average, m.Value)
+		stateNext.StdDev = algorithm.Ewms(wf, state.Average, stateNext.Average, state.StdDev, m.Value)
+		if state.Count < startSize {
+			stateNext.Count = state.Count + 1
+		} else {
+			stateNext.Count = state.Count
+		}
+	}
+	if stateNext.Count >= startSize {
+		m.Score = algorithm.Div3Sigma(stateNext.Average, stateNext.StdDev, m.Value)
+	} else {
+		m.Score = 0
+	}
+	err = detector.db.PutState(m, stateNext)
+	return err
 }
