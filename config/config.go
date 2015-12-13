c12d28e5f36ee31808a9e35b6a757d1a63daa5eb
hit9 <hit9@icloud.com>
2015-12-13 17:04:56 +0800

    Use default gridLen as 5min and use default TrendFactor as 5min

--- config/config.go
+++ config/config.go
@@ -4,21 +4,21 @@
 //  Global Options
 //    interval         incomding metrics time interval (in sec). [default: 10]
 //    periodicity      metrics periodicity (in sec), NumTimeGrids x TimeGridLen.
-//                     [default: [480, 1800], aka 480x0.5h]
+//                     [default: [288, 300], aka 288x5min]
 //  Storage Options
 //    path             path for leveldb, which maintains analyzation
 //                     results. [default: "storage/"]
 //  Detector Options
 //    port             detector tcp port to listen. [default: 2015]
 //    trendFactor      the factor to calculate trending value via weighted
-//                     moving average algorithm. [default: 0.07]
+//                     moving average algorithm. [default: 0.05]
 //    strict           if this is set false, detector will passivate latest
 //                     metric. [default: true]
 //    blacklist        metrics blacklist, detector will allow one metric to pass
 //                     only if it matches one pattern in rules and dosent match
 //                     any pattern in blacklist. [default: ["statsd.*"]]
 //    startSize        detector won't start to detect until the data set is
-//                     larger than this size. [default: 30, aka 5min]
+//                     larger than this size. [default: 18, aka 3min]
 //  WebApp Options
 //    port             webapp http port to listen. [default: 2016]
 //    auth             username and password for admin basic auth. [default:
@@ -69,13 +69,13 @@ type ConfigAlerter struct {
 func NewWithDefaults() *Config {
 	config := new(Config)
 	config.Interval = 10
-	config.Periodicity = [2]int{480, 1800}
+	config.Periodicity = [2]int{288, 300}
 	config.Storage.Path = "storage/"
 	config.Detector.Port = 2015
-	config.Detector.TrendFactor = 0.07
+	config.Detector.TrendFactor = 0.05
 	config.Detector.Strict = true
 	config.Detector.BlackList = []string{"statsd.*"}
-	config.Detector.StartSize = uint32(30)
+	config.Detector.StartSize = uint32(18)
 	config.Webapp.Port = 2016
 	config.Webapp.Auth = [2]string{"admin", "admin"}
 	config.Alerter.Command = ""
