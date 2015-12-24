9db836a0c16a75264a44518d3ad27e7ca82183de
xczhang <xuchuan.zhang@ele.me>
2015-12-24 17:24:14 +0800

    Use suffix tree in hitCache

--- detector/cache_test.go
+++ detector/cache_test.go
@@ -23,12 +23,4 @@ func TestCache(t *testing.T) {
 	e, v = c.hitCache(mc)
 	assert.Ok(t, !e)
 
-	c.updateRules()
-	e, v = c.hitCache(ma)
-	assert.Ok(t, !e)
-	e, v = c.hitCache(mb)
-	assert.Ok(t, !e)
-	c.setCache(mc, true)
-	e, v = c.hitCache(mc)
-	assert.Ok(t, e && v)
 }
