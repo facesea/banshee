d5dd9c98332ebfb1ab5689774e23b0760da7e1f6
hit9 <hit9@icloud.com>
2015-12-24 23:43:39 +0800

    Rename exampleCommand to alerter/exampleCommand

--- alerter/exampleCommand/echo.go
+++ alerter/exampleCommand/echo.go
@@ -1,6 +1,6 @@
 // Copyright 2015 Eleme Inc. All rights reserved.
 
-// exampleAlert.go is a script which will alert msg out by banshee
+// Example alerter command to echo message to console.
 package main
 
 import (
@@ -21,14 +21,14 @@ func main() {
 	str := os.Args[1]
 	res := msg{}
 	json.Unmarshal([]byte(str), &res)
-
+	// Email
 	if res.User.EnableEmail {
 		fmt.Printf("Alert metric - project:%s metric:%s value:%5f average:%5f score:%5f timestamp:%d",
 			res.Project.Name, res.Metric.Name, res.Metric.Value, res.Metric.Average, res.Metric.Score,
 			res.Metric.Stamp)
 		fmt.Printf("User info - name:%s email:%s", res.User.Name, res.User.Email)
 	}
-
+	// Phone
 	if res.User.EnablePhone {
 		fmt.Printf("Alert metric - project:%s metric:%s value:%5f average:%5f score:%5f timestamp:%d",
 			res.Project.Name, res.Metric.Name, res.Metric.Value, res.Metric.Average, res.Metric.Score,
