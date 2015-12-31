6a8c21aa160b56b025103c0ddb43c775d59c2776
hit9 <hit9@icloud.com>
2015-12-31 11:57:50 +0800

    Statsd: Fix match and options errors

--- statsd/index.js
+++ statsd/index.js
@@ -1,41 +1,55 @@
-/**
- * Backend for github.com/etsy/statsd.
- *
- * Optional configs:
- *   bansheeHost         banshee host to connect to. [default: '0.0.0.0']
- *   bansheePort         banshee port to connect to. [default: 2015]
- *   bansheeIgnores      wildcard patterns to ignore stats. [default: ['statsd.*']]
- *   bansheeTimerFields  timer data fields to forward. [default: ['mean_90', 'count_ps']]
- *
- * Supported metric types:
- *
- *   counter_rates, timers, gauges
- */
+// Backend for github.com/etsy/statsd.
+//
+// Optional configs:
+//   bansheeHost         banshee host to connect to. [default: '0.0.0.0']
+//   bansheePort         banshee port to connect to. [default: 2015]
+//   bansheeIgnores      wildcard patterns to ignore stats. [default: ['statsd.*']]
+//   bansheeTimerFields  timer data fields to forward. [default: ['mean_90', 'count_ps']]
+//
+// Supported metric types:
+//
+//   counter_rates, timers, gauges
 
 'use strict';
 
 var net = require('net');
-var  minimatch = require('minimatch');
+var util = require('util');
 
+// Match string with pattern.
+function match(p, s) {
+  var arr = p.split("*");
+  var i, j;
+  for (i = 0, j = 0; i < arr.length; i++) {
+    j = s.indexOf(arr[i], j);
+    if (j < 0)
+      return false;
+    j += arr[i].length;
+  }
+  return true;
+}
+
+// Globals
 var config;
 var debug;
 var logger;
 
+// Metric type to makers mappings.
 var makers =  {
   'counter_rates': dataFromCounterRates,
   'timer_data': dataFromTimerData,
   'gauges': dataFromGauges,
 };
 
+// Make data from counter_rates.
 function dataFromCounterRates(key, val, timeStamp) {
   var name = util.format('counter.%s', key);
   return [[name, timeStamp, val]];
 }
 
+// Make data from timers.
 function dataFromTimerData(key, dict, timeStamp) {
   var fields = config.bansheeTimerFields || ['mean_90', 'count_ps'];
   var data = [];
-
   for (var i = 0; i < fields.length; i++) {
       var field = fields[i];
       var name = util.format('timer.%s.%s', field, key);
@@ -44,24 +58,29 @@ function dataFromTimerData(key, dict, timeStamp) {
   return data
 }
 
+// Make data from gauges.
 function dataFromGauges(key, val, timeStamp) {
   var name = util.format('gauge.%s', key);
   return [[name, timeStamp, val]];
 }
 
-function Banshee(options) {}
+// Banshee.
+function Banshee() {}
 
+// Connect to banshee server.
 Banshee.prototype.connect = function(cb) {
   var self = this;
   var options = {
-    host: config.bansheeHost,
-    port: config.bansheePort,
+    host: config.bansheeHost || '0.0.0.0',
+    port: config.bansheePort || 2015,
   };
   this.conn = net.connect(options, function() {
+    // Ok
     if (debug)
       logger.log('banshee conneted');
     cb && cb();
   }).on('error', function(err) {
+    // Error
     if (debug)
       logger.log('banshee socket error: ' + err.message + ', disconnecting..');
     self.conn.destroy();
@@ -70,24 +89,29 @@ Banshee.prototype.connect = function(cb) {
   return this;
 };
 
+// Match metric.
 Banshee.prototype.match = function(key) {
   var patterns = config.bansheeIgnores || ['statsd.*'];
   for (var i = 0; i < patterns.length; i++)
-    if (minimatch(key, patterns[i]))
+    if (match(patterns[i], key))
       return true;
   return false;
 };
 
+// Send buffer.
 Banshee.prototype.send = function(buf, cb) {
   var self = this;
   if (!this.conn)
+    // Auto connect
     return this.connect(function() {
       return self.conn.write(buf, cb);
     });
   return this.conn.write(buf, cb);
 };
 
+// Flush metrics.
 Banshee.prototype.flush = function(timeStamp, data) {
+  // Collect data.
   var list = [];
   var types = Object.keys(makers);
 
@@ -103,22 +127,20 @@ Banshee.prototype.flush = function(timeStamp, data) {
       }
     }
   }
-
+  // Make and send buffer.
   if (list.length > 0) {
     var buf = '';
     for (i = 0; i < list.length; i++)
       buf += list[i].join(' ') + '\n';
+      // Send
       this.send(buf, function() {
-        if (debug) {
-          var msg = util.format("sent to banshee: %s", JSON.stringify(list[0]));
-          if (list.length > 1)
-            msg = util.format("%s, (%d more..)", msg, list.length - 1)
-          logger.log(msg);
-        }
+        if (debug)
+          logger.log(util.format("sent to banshee %d metrics", list.length));
       });
   }
 };
 
+// Export init.
 exports.init = function(uptime, _config, events, _logger) {
   logger = _logger || console;
   debug = _config.debug;
