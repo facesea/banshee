bf659f33ae3cf87bd01ff7d60c3a0a92b4890fec
panda <facesea18@gmail.com>
2016-01-12 18:50:54 +0800

    Add Rule

--- static/src/components/RuleList.js
+++ static/src/components/RuleList.js
@@ -11,6 +11,10 @@ import RaisedButton from 'material-ui/lib/raised-button'
 import IconButton from 'material-ui/lib/icon-button'
 import List from 'material-ui/lib/lists/list'
 import ListItem from 'material-ui/lib/lists/list-item'
+import FlatButton from 'material-ui/lib/flat-button'
+import TextField from 'material-ui/lib/text-field'
+import Dialog from 'material-ui/lib/dialog'
+import Checkbox from 'material-ui/lib/checkbox'
 
 import ActionDelete from 'material-ui/lib/svg-icons/action/delete'
 
@@ -21,14 +25,53 @@ import ruleStyles from '../styles/rule.scss'
 const mapStateToProps = (state) => ({
   project: state.projectDetail.project,
   rules: state.projectDetail.rules,
-  id: state.router.path.split('/')[1]
+  id: state.router.path.split('/')[1],
+  ruleOpen: state.projectDetail.ruleOpen,
+  submitDisabled: state.projectDetail.submitDisabled,
+  patternErrorText: state.projectDetail.patternErrorText,
+  onTrendingUp: state.projectDetail.onTrendingUp,
+  onTrendingDown: state.projectDetail.onTrendingDown,
+  onValueGt: state.projectDetail.onValueGt,
+  onValueLt: state.projectDetail.onValueLt,
+  onTrendingUpAndValueGt: state.projectDetail.onTrendingUpAndValueGt,
+  onTrendingDownAndValueLt: state.projectDetail.onTrendingDownAndValueLt,
+  thresholdMax: state.projectDetail.thresholdMax,
+  thresholdMin: state.projectDetail.thresholdMin,
+  trustline: state.projectDetail.trustline,
+  thresholdMaxErrorText: state.projectDetail.thresholdMaxErrorText,
+  thresholdMinErrorText: state.projectDetail.thresholdMinErrorText,
+  trustlineErrorText: state.projectDetail.trustlineErrorText
 })
 
 export class RuleList extends React.Component {
   static propTypes = {
     getProjectById: React.PropTypes.func.isRequired,
     getAllRules: React.PropTypes.func.isRequired,
-    id: React.PropTypes.string.isRequired
+    ruleDialogClose: React.PropTypes.func.isRequired,
+    id: React.PropTypes.string.isRequired,
+    ruleOpen: React.PropTypes.bool.isRequired,
+    addRule: React.PropTypes.func.isRequired,
+    project: React.PropTypes.object.isRequired,
+
+    handlePatternChange: React.PropTypes.func.isRequired,
+    handleCheck: React.PropTypes.func.isRequired,
+    handleInput: React.PropTypes.func.isRequired,
+
+    submitDisabled: React.PropTypes.bool.isRequired,
+    onTrendingUp: React.PropTypes.bool.isRequired,
+    onTrendingDown: React.PropTypes.bool.isRequired,
+    onValueGt: React.PropTypes.bool.isRequired,
+    onValueLt: React.PropTypes.bool.isRequired,
+    onTrendingUpAndValueGt: React.PropTypes.bool.isRequired,
+    onTrendingDownAndValueLt: React.PropTypes.bool.isRequired,
+    thresholdMax: React.PropTypes.number,
+    thresholdMin: React.PropTypes.number,
+    trustline: React.PropTypes.number,
+
+    patternErrorText: React.PropTypes.string.isRequired,
+    thresholdMaxErrorText: React.PropTypes.string.isRequired,
+    thresholdMinErrorText: React.PropTypes.string.isRequired,
+    trustlineErrorText: React.PropTypes.string.isRequired
   }
 
   componentDidMount () {
@@ -63,11 +106,24 @@ export class RuleList extends React.Component {
       </IconButton>
     )
 
+    const ruleActions = [
+      <FlatButton
+        label='Cancel'
+        secondary
+        onTouchTap={this.props.ruleDialogClose} />,
+      <FlatButton
+        label='Submit'
+        primary
+        form='form'
+        disabled={this.props.submitDisabled}
+        onTouchTap={this.props.addRule} />
+    ]
+
     return (
       <Paper zDepth={1}>
         <Toolbar style={styles.toolbar}>
           <ToolbarGroup float='left'>
-            <ToolbarTitle text='Project Detail' />
+            <ToolbarTitle text={this.props.project.name} />
             <ToolbarSeparator style={styles.separator}/>
             <ToolbarTitle text='Rules' />
           </ToolbarGroup>
@@ -83,6 +139,78 @@ export class RuleList extends React.Component {
             secondaryText='Change your Google+ profile photo'
             rightIconButton={rightIconButton}/>
         </List>
+        <Dialog
+          title='Add Rule'
+          actions={ruleActions}
+          modal={false}
+          open
+          onRequestClose={this.props.ruleDialogClose}>
+            <form id='form' onSubmit={this.props.addRule}>
+              <div className={ruleStyles.row}>
+                <label className={ruleStyles.label}>Pattern:</label>
+                <div className={ruleStyles.rightPart}>
+                  <TextField
+                    className={ruleStyles.verticalAlign}
+                    hintText='timer.count_ps.*'
+                    onChange={this.props.handlePatternChange}
+                    errorText={this.props.patternErrorText}/>
+                </div>
+              </div>
+
+              <div className={ruleStyles.row}>
+                <label className={ruleStyles.label}>Alerting:</label>
+                <div className={ruleStyles.rightPart}>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onTrendingUp', checked) }}
+                    label='On trending up'/>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onTrendingDown', checked) }}
+                    label='On trending down'/>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onValueGt', checked) }}
+                    label='On value >= thresholdMax'/>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onValueLt', checked) }}
+                    label='On value <= thresholdMin'/>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onTrendingUpAndValueGt', checked) }}
+                    label='On trending up and value >= thresholdMax'/>
+                  <Checkbox
+                    onCheck={(e, checked) => { this.props.handleCheck('onTrendingDownAndValueLt', checked) }}
+                    label='On trending down and value <= thresholdMin'/>
+                  <div>
+                    <label>thresholdMax:</label>
+                    <TextField
+                      value={this.props.thresholdMax}
+                      className={ruleStyles.smallField}
+                      onChange={(e) => { this.props.handleInput('thresholdMax', e.target.value) }}
+                      errorText={this.props.thresholdMaxErrorText}/>
+                  </div>
+                  <div>
+                    <label>thresholdMin:</label>
+                    <TextField
+                      value={this.props.thresholdMin}
+                      className={ruleStyles.smallField}
+                      onChange={(e) => { this.props.handleInput('thresholdMin', e.target.value) }}
+                      errorText={this.props.thresholdMinErrorText}/>
+                  </div>
+                </div>
+              </div>
+
+              <div className={ruleStyles.row}>
+                <label className={ruleStyles.label}>Trustline:</label>
+                <div className={ruleStyles.rightPart && ruleStyles.divVertical}>
+                  <label>Don't alert me when value is less than</label>
+                  <TextField
+                      value={this.props.trustline}
+                      className={ruleStyles.smallField}
+                      onChange={(e) => { this.props.handleInput('trustline', e.target.value) }}
+                      errorText={this.props.trustlineErrorText}/>
+                </div>
+              </div>
+
+            </form>
+        </Dialog>
       </Paper>
     )
   }
--- static/src/redux/modules/projectDetail.js
+++ static/src/redux/modules/projectDetail.js
@@ -10,10 +10,29 @@ import request from 'superagent'
 export const SET_ALL_RULES = 'SET_ALL_RULES'
 export const GET_ALL_RULES = 'GET_ALL_RULES'
 export const SET_PROJECT = 'SET_PROJECT'
+export const RULE_DIALOG_CLOSE = 'RULE_DIALOG_CLOSE'
+export const ADD_RULE = 'ADD_RULE'
+export const HANDLE_PATTERN_CHANGE = 'HANDLE_PATTERN_CHANGE'
+export const HANDLE_CHECK = 'HANDLE_CHECK'
+export const HANDLE_INPUT = 'HANDLE_INPUT'
+export const ADD_RULE_SUCCESS = 'ADD_RULE_SUCCESS'
+export const ADD_RULE_FAIL = 'ADD_RULE_FAIL'
 
 export const INIT_STATE = {
   project: {},
-  rules: []
+  rules: [],
+  ruleOpen: false,
+  submitDisabled: false,
+  onTrendingUp: false,
+  onTrendingDown: false,
+  onValueGt: false,
+  onValueLt: false,
+  onTrendingUpAndValueGt: false,
+  onTrendingDownAndValueLt: false,
+  patternErrorText: '',
+  thresholdMaxErrorText: '',
+  thresholdMinErrorText: '',
+  trustlineErrorText: ''
 }
 
 // ------------------------------------
@@ -21,6 +40,12 @@ export const INIT_STATE = {
 // ------------------------------------
 export const setProject = createAction(SET_PROJECT, (project) => project)
 export const setAllRules = createAction(SET_ALL_RULES, (rules) => rules)
+export const ruleDialogClose = createAction(RULE_DIALOG_CLOSE, () => false)
+export const handlePatternChange = createAction(HANDLE_PATTERN_CHANGE, (e) => e.target.value)
+export const handleCheck = createAction(HANDLE_CHECK, (name, checked) => ({name: name, checked: checked}))
+export const handleInput = createAction(HANDLE_INPUT, (name, val) => ({name: name, val: val}))
+export const addRuleFail = createAction(ADD_RULE_FAIL, () => false)
+export const addRuleSuccess = createAction(ADD_RULE_SUCCESS, (rule) => rule)
 
 export const getProjectById = (id) => {
   return (dispatch, getState) => {
@@ -50,9 +75,43 @@ export const getAllRules = (id) => {
   }
 }
 
+export const addRule = () => {
+  return (dispatch, getState) => {
+    const state = getState()
+    const params = {
+      'pattern': state.pattern,
+      'projectID': state.id,
+      'onTrendingUp': state.onTrendingUp,
+      'onTrendingDown': state.onTrendingDown,
+      'onValueGt': state.onValueGt,
+      'onValueLt': state.onValueLt,
+      'onTrendingUpAndValueGt': state.onTrendingUpAndValueGt,
+      'onTrendingDownAndValueLt': state.onTrendingDownAndValueLt,
+      'thresholdMax': state.thresholdMax,
+      'thresholdMin': state.thresholdMin,
+      'trustline': state.trustline
+    }
+    request
+      .post('/api/rule')
+      .send(params)
+      .end(function (err, res) {
+        if (err || !res.ok) {
+          dispatch(addRuleFail(err.msg))
+        } else {
+          dispatch(addRuleSuccess(res.body))
+        }
+      })
+  }
+}
 export const actions = {
   getProjectById,
-  getAllRules
+  getAllRules,
+  ruleDialogClose,
+  addRule,
+
+  handlePatternChange,
+  handleCheck,
+  handleInput
 }
 
 // ------------------------------------
@@ -68,9 +127,73 @@ export default handleActions({
     return Object.assign({}, state, {
       rules: payload
     })
+  },
+  [RULE_DIALOG_CLOSE]: (state, { payload }) => {
+    return Object.assign({}, state, {
+      ruleOpen: payload
+    })
+  },
+  [HANDLE_PATTERN_CHANGE]: (state, { payload }) => {
+    if (!payload) {
+      state.patternErrorText = 'This field is required.'
+    } else {
+      state.patternErrorText = ''
+    }
+
+    validate(state)
+
+    return Object.assign({}, state, {
+      pattern: payload
+    })
+  },
+  [HANDLE_CHECK]: (state, { payload }) => {
+    state[payload.name] = payload.checked
+    return Object.assign({}, state)
+  },
+  [HANDLE_INPUT]: (state, { payload }) => {
+    if (Number(payload.val)) {
+      state[payload.name] = payload.val
+      state[payload.name + 'ErrorText'] = ''
+    } else {
+      state[payload.name] = undefined
+      state[payload.name + 'ErrorText'] = 'This field must be numeric.'
+    }
+
+    validate(state)
+
+    return Object.assign({}, state)
+  },
+  [ADD_RULE_SUCCESS]: (state, { payload }) => {
+    return Object.assign({}, state, {
+      rules: [
+        ...state.rules,
+        payload
+      ],
+      ruleOpen: false,
+      submitDisabled: false,
+      onTrendingUp: false,
+      onTrendingDown: false,
+      onValueGt: false,
+      onValueLt: false,
+      onTrendingUpAndValueGt: false,
+      onTrendingDownAndValueLt: false,
+      patternErrorText: '',
+      thresholdMaxErrorText: '',
+      thresholdMinErrorText: '',
+      trustlineErrorText: ''
+    })
   }
 },
 
 // init state
 INIT_STATE
 )
+
+// validate form
+function validate (state) {
+  if (state.patternErrorText || state.thresholdMaxErrorText || state.thresholdMinErrorText || state.trustlineErrorText) {
+    state.submitDisabled = true
+  } else {
+    state.submitDisabled = false
+  }
+}
