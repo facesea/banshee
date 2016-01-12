e1c365b116dd7d920c4ea243f430b10d2cc069ac
hit9 <hit9@icloud.com>
2016-01-12 18:25:12 +0800

    Util: Fix `ToFixed` for `100` like integers

--- util/util_test.go
+++ util/util_test.go
@@ -13,6 +13,7 @@ func TestToFixed(t *testing.T) {
 	assert.Ok(t, ToFixed(10000.12121121, 5) == "10000.12121")
 	assert.Ok(t, ToFixed(102, 3) == "102")
 	assert.Ok(t, ToFixed(102.22, 3) == "102.22")
+	assert.Ok(t, ToFixed(100, 3) == "100")
 }
 
 func TestIsFileExist(t *testing.T) {
