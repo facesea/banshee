b107d534c5762b969395a52964687ad6599629ea
panda <facesea18@gmail.com>
2016-01-15 18:43:25 +0800

    Fix create project not show

--- static/src/modules/admin/project/AdminProjectListCtrl.js
+++ static/src/modules/admin/project/AdminProjectListCtrl.js
@@ -1,10 +1,6 @@
 /*@ngInject*/
 module.exports = function ($scope, $modal, $mdDialog, Project) {
 
-  /**
-   * 加载数据
-   * @param
-   */
   $scope.loadData = function () {
     Project.getAllProjects().$promise
       .then(function (res) {
@@ -12,17 +8,7 @@ module.exports = function ($scope, $modal, $mdDialog, Project) {
       });
   }
 
-  /**
-   * 打开创建弹框
-   * @param
-   */
   $scope.openModal = function (event) {
-    // $modal.open({
-    //   templateUrl: 'modules/admin/project/projectModal.html',
-    //   controller: 'ProjectModalCtrl'
-    // }).result.then(function (project) {
-    //   $scope.projects.push(project);
-    // })
     $mdDialog.show({
       controller: 'ProjectModalCtrl',
       templateUrl: 'modules/admin/project/projectModal.html',
@@ -30,6 +16,9 @@ module.exports = function ($scope, $modal, $mdDialog, Project) {
       targetEvent: event,
       clickOutsideToClose:true,
       fullscreen: true
+    })
+    .then(function (project) {
+      $scope.projects.push(project);
     })
   }
 
