b140e5515b9430d411eef0b97d4610ef5eb8ddff
hit9 <hit9@icloud.com>
2016-01-16 00:01:54 +0800

    Remove chinese comments

--- static/src/modules/admin/project/ProjectModalCtrl.js
+++ static/src/modules/admin/project/ProjectModalCtrl.js
@@ -7,11 +7,7 @@ module.exports = function ($scope, toastr, $mdDialog, Project) {
   $scope.cancel = function() {
     $mdDialog.cancel();
   }
-  
-  /**
-   * 提交创建表单
-   * @param
-   */
+
   $scope.create = function() {
     Project.save($scope.project).$promise
       .then(function(res) {
