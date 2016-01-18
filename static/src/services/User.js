16a924b58699ada66db02cd8720e59a930229841
panda <facesea18@gmail.com>
2016-01-18 15:29:13 +0800

    Change getProjectsByUserId url

--- static/src/services/User.js
+++ static/src/services/User.js
@@ -11,7 +11,7 @@ module.exports = function ($resource) {
     },
     getProjectsByUserId: {
       method: 'GET',
-      url: '/api/projects',
+      url: '/api/user/:id/projects',
       isArray: true
     },
     edit: {
