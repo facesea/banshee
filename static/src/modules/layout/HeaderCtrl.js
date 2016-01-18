4a0299475f1d3f91e07b274b0bbeab88ca90f292
panda <facesea18@gmail.com>
2016-01-18 17:04:48 +0800

    Change header link

--- static/src/modules/layout/HeaderCtrl.js
+++ static/src/modules/layout/HeaderCtrl.js
@@ -2,5 +2,8 @@
  * Created by Panda on 16/1/13.
  */
 /*@ngInject*/
-module.exports = function () {
+module.exports = function ($scope, $state) {
+  $scope.includes = function(state) {
+    return $state.includes(state);
+  };
 };
