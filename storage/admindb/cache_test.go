6afd7e8a95205c20a1b96a60087e3bce07520eea
hit9 <hit9@icloud.com>
2016-01-19 20:25:37 +0800

    Add more strict test for RulesCache init

--- storage/admindb/cache_test.go
+++ storage/admindb/cache_test.go
@@ -14,15 +14,25 @@ func TestInit(t *testing.T) {
 	db, _ := Open(fileName)
 	defer db.Close()
 	defer os.RemoveAll(fileName)
-	rule := &models.Rule{Pattern: "a.b.*"}
-	// Add one to db.
-	db.DB().Create(rule)
+	rule1 := &models.Rule{Pattern: "a.b.*"}
+	rule2 := &models.Rule{Pattern: "a.b.*.c"}
+	rule3 := &models.Rule{Pattern: "a.*.c.d"}
+	// Add to db.
+	db.DB().Create(rule1)
+	db.DB().Create(rule2)
+	db.DB().Create(rule3)
 	// Clear cache.
 	db.RulesCache.rules.Clear()
 	// Reload
 	assert.Ok(t, nil == db.RulesCache.Init(db.DB()))
 	// Get rule
-	r, ok := db.RulesCache.Get(rule.ID)
+	r1, ok := db.RulesCache.Get(rule1.ID)
 	assert.Ok(t, ok)
-	assert.Ok(t, r.Pattern == rule.Pattern)
+	assert.Ok(t, r1.Pattern == rule1.Pattern)
+	r2, ok := db.RulesCache.Get(rule2.ID)
+	assert.Ok(t, ok)
+	assert.Ok(t, r2.Pattern == rule2.Pattern)
+	r3, ok := db.RulesCache.Get(rule3.ID)
+	assert.Ok(t, ok)
+	assert.Ok(t, r3.Pattern == rule3.Pattern)
 }
