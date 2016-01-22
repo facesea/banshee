628c3a35e8c096f2bb6f061e89dd04a957c1643b
hit9 <hit9@icloud.com>
2016-01-22 15:32:25 +0800

    Add tab items 3-7 day to DateTimes

--- static/src/constants/DateTimes.js
+++ static/src/constants/DateTimes.js
@@ -18,5 +18,25 @@ module.exports = [
 	{
 		label: '2 day ago',
 		seconds: 48 * 3600
-	}
+	},
+  {
+    label: '3 day ago',
+    seconds: 3 * 24 * 3600
+  },
+  {
+    label: '4 day ago',
+    seconds: 4 * 24 * 3600
+  },
+  {
+    label: '5 day ago',
+    seconds: 5 * 24 * 3600
+  },
+  {
+    label: '6 day ago',
+    seconds: 6 * 24 * 3600
+  },
+  {
+    label: '7 day ago',
+    seconds: 7 * 24 * 3600
+  }
 ];
