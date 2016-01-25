1c715c7b6278c6c3671beea852a78f1a6f09b768
hit9 <hit9@icloud.com>
2016-01-25 11:15:54 +0800

    Remove models.State

--- models/doc.go
+++ models/doc.go
@@ -1,5 +1,4 @@
 // Copyright 2015 Eleme Inc. All rights reserved.
 
-// Package models implements metric, index, rule, project, user and state
-// modeling.
+// Package models implements metric, index, rule, project, user modeling.
 package models
