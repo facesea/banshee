afb48728b4a5c7d4e06103e5d0e1f48db56a87f6
hit9 <hit9@icloud.com>
2016-01-29 14:07:21 +0800

    Retabe js tabs

--- static/src/constants/AdminNavList.js
+++ static/src/constants/AdminNavList.js
@@ -1,16 +1,16 @@
 module.exports = [
-	{
-		name: 'Project',
-		state: 'banshee.admin.project'
-	},
-	{
-		name: 'Receiver',
-		state: 'banshee.admin.user'
-	},
-	{
-		name: 'Config',
-		state: 'banshee.admin.config'
-	},
+  {
+    name: 'Project',
+    state: 'banshee.admin.project'
+  },
+  {
+    name: 'Receiver',
+    state: 'banshee.admin.user'
+  },
+  {
+    name: 'Config',
+    state: 'banshee.admin.config'
+  },
   {
     name: 'Info',
     state: 'banshee.admin.info'
--- static/src/constants/DateTimes.js
+++ static/src/constants/DateTimes.js
@@ -1,24 +1,24 @@
 module.exports = [
-	{
-		label: 'Now',
-		seconds: 0
-	},
-	{
-		label: '3 hours ago',
-		seconds: 3 * 3600
-	},
-	{
-		label: '6 hours ago',
-		seconds: 6 * 3600
-	},
-	{
-		label: '1 day ago',
-		seconds: 24 * 3600
-	},
-	{
-		label: '2 day ago',
-		seconds: 48 * 3600
-	},
+  {
+    label: 'Now',
+    seconds: 0
+  },
+  {
+    label: '3 hours ago',
+    seconds: 3 * 3600
+  },
+  {
+    label: '6 hours ago',
+    seconds: 6 * 3600
+  },
+  {
+    label: '1 day ago',
+    seconds: 24 * 3600
+  },
+  {
+    label: '2 day ago',
+    seconds: 48 * 3600
+  },
   {
     label: '3 day ago',
     seconds: 3 * 24 * 3600
