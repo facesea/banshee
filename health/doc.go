e375c1817ed3e2a0da779e14db0f717dfadceba6
hit9 <hit9@icloud.com>
2016-02-01 17:03:46 +0800

    Show health data in table

--- health/doc.go
+++ health/doc.go
@@ -9,12 +9,10 @@ Package health implements the health statistic aggregation.
 	numIndexTotal                 // Number of metric indexes total.
 	numClients                    // Number of detector clients.
 	// Aggregation
-	detectionCost                 // Average of detection time cost in last interval.
+	detectionCost                 // Average of detection time cost (in milliseconds) in last interval.
 	numMetricIncomed              // Number of metrics incomed in last interval.
 	numMetricDetected             // Number of metrics detected in last interval.
 	numAlertingEvents             // Number of alerting events in last interval.
-
-You can visit /api/info for health info.
 
 */
 package health
