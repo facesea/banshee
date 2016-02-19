212a418061bed097287f7244e1e252841792fa03
hit9 <hit9@icloud.com>
2016-02-19 15:10:34 +0800

    Don't edit rule fields in util buildRepr Note: this may cause unexcepted behaviors

--- static/src/services/Util.js
+++ static/src/services/Util.js
@@ -2,29 +2,30 @@
 module.exports = function () {
   return {
     buildRepr: function (rule) {
-      rule.trendUp = rule.trendUp || false;
-      rule.trendDown = rule.trendDown || false;
-      rule.thresholdMax = rule.thresholdMax || 0;
-      rule.thresholdMin = rule.thresholdMin || 0;
-
       var parts = [];
-      if (rule.trendUp && rule.thresholdMax === 0) {
+
+      trendUp = rule.trendUp || false;
+      trendDown = rule.trendDown || false;
+      thresholdMax = rule.thresholdMax || 0;
+      thresholdMin = rule.thresholdMin || 0;
+
+      if (trendUp && thresholdMax === 0) {
         parts.push('trend ↑');
       }
-      if (rule.trendUp && rule.thresholdMax !== 0) {
-        parts.push('(trend ↑ && value >= ' + parseFloat(rule.thresholdMax.toFixed(3)) + ')');
+      if (trendUp && thresholdMax !== 0) {
+        parts.push('(trend ↑ && value >= ' + parseFloat(thresholdMax.toFixed(3)) + ')');
       }
-      if (!rule.trendUp && rule.thresholdMax !== 0) {
-        parts.push('value >= ' + parseFloat(rule.thresholdMax.toFixed(3)));
+      if (!trendUp && thresholdMax !== 0) {
+        parts.push('value >= ' + parseFloat(thresholdMax.toFixed(3)));
       }
-      if (rule.trendDown && rule.thresholdMin === 0) {
+      if (trendDown && thresholdMin === 0) {
         parts.push('trend ↓');
       }
-      if (rule.trendDown && rule.thresholdMin !== 0) {
-        parts.push('(trend ↓ && value <= ' + parseFloat(rule.thresholdMin.toFixed(3)) + ')');
+      if (trendDown && thresholdMin !== 0) {
+        parts.push('(trend ↓ && value <= ' + parseFloat(thresholdMin.toFixed(3)) + ')');
       }
-      if (!rule.trendDown && rule.thresholdMin !== 0) {
-        parts.push('value <= ' + parseFloat(rule.thresholdMin.toFixed(3)));
+      if (!trendDown && thresholdMin !== 0) {
+        parts.push('value <= ' + parseFloat(thresholdMin.toFixed(3)));
       }
       return parts.join(' || ');
     }
