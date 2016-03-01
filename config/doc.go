1f29059cd9a416685fe13f9311525673dac6db8c
hit9 <hit9@icloud.com>
2016-03-01 11:14:03 +0800

    Add config item `Webapp.Notice` (map)

--- config/doc.go
+++ config/doc.go
@@ -29,7 +29,8 @@ Configuration is in JSON file, for example:
 	  "webapp": {
 	    "port": 2016,
 	    "auth": ["user", "pass"],
-	    "static": "static/dist"
+	    "static": "static/dist",
+		"notice": {}
 	  },
 	  "alerter": {
 	    "command": "",
@@ -68,6 +69,7 @@ The documents for each configuration item with default values:
 	webapp.port                    // Webapp http port to listen.
 	webapp.auth                    // Webapp admin pages basic auth, in form of [user, pass], use empty string ["", ""] to disable auth. default: ["admin", "admin"]
 	webapp.static                  // Webapp static files (htmls/css/js) path, default: "static/dist"
+	webapp.notice                  // Webapp notice in HTML, default: {}, example: {"docs": "url-to-docs"}
 	alerter.command                // Alerter command or script to execute on anomalies found. default: ""
 	alerter.workers                // Number of workers to consume command execution jobs. default: 4
 	alerter.interval               // Minimal interval (in seconds) between two alerting message for one metric. default: 1200
