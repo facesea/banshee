5926ae3d0d867d87774839659222f3e460129cc0
hit9 <hit9@icloud.com>
2016-03-03 11:58:27 +0800

    Add api getLanguage to get webapp perfered language

--- webapp/server.go
+++ webapp/server.go
@@ -43,6 +43,7 @@ func Start(c *config.Config, d *storage.DB, f *filter.Filter) {
 	router.GET("/api/config", auth.handler(getConfig))
 	router.GET("/api/interval", getInterval)
 	router.GET("/api/notice", getNotice)
+	router.GET("/api/language", getLanguage)
 	router.GET("/api/projects", getProjects)
 	router.GET("/api/project/:id", getProject)
 	router.POST("/api/project", auth.handler(createProject))
